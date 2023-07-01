1e5797d21e2cddde61c858724d3aea4ecf09359e
Steve <git@github.com:SKaranjaN/Database.git>
2023-07-01 12:25:13 +0300

    table model

--- server/models.py
+++ server/models.py
@@ -1,1 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class Country(db.Model, SerializerMixin):
+    __tablename__ = "countries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    population =db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<country: {self.name}, population: {self.population}'
